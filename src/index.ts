a1658da9b20d16dc3c3e9006fb7df6bdba08afad
Luccas Oliveira Marinho da Silva <luccas88oliveira@gmail.com>
2025-05-28 17:52:18 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import express from "express";
+const app = express();
+
+app.use(express.json());
+
+const port: number = 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`))
